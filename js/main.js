8df70a4b65a6936b8e88f4bd54e301157eb2565f
Stefano De Capitani <stefano.deca@live.com>
2021-10-20 11:52:09 +0200

    First part completed

--- js/main.js
+++ js/main.js
@@ -1,13 +1,71 @@
-let gridContainer = document.querySelector(".minefield");
+let playButton = document.querySelector(".btn--play");
 
-for (let i = 0; i < 100; i++) {
+createNewLevel();
+
+playButton.addEventListener("click", createNewLevel);
+
+function createNewLevel() {
+  let levelSelect = document.querySelector("#level-select");
+
+  newLevel = parseInt(levelSelect.value);
+  numberOfCells = getHowManyCellsNeededFor(newLevel);
+
+  setNumberOfColumns(numberOfCells);
+  createCells(numberOfCells);
+  addEventListenerToCells();
+}
+
+function getHowManyCellsNeededFor(newLevel) {
+  const cellsPerLevel = [100, 81, 49];
+  let numberOfCells;
+  for (let i = 0; i < cellsPerLevel.length; i++) {
+    if (i === newLevel) {
+      numberOfCells = cellsPerLevel[i];
+    }
+  }
+  return numberOfCells;
+}
+
+function setNumberOfColumns(numberOfCells) {
+  let gridContainer = document.querySelector(".minefield");
+
+  let numberOfColumns = Math.sqrt(numberOfCells);
+
+  gridContainer.style.gridTemplateColumns = `repeat(${numberOfColumns}, 1fr)`;
+}
+
+function createCells(numberOfCells) {
+  let gridContainer = document.querySelector(".minefield");
+  gridContainer.textContent = "";
+
+  for (let i = 0; i < numberOfCells; i++) {
+    let cell = createNewSingleCell(i);
+    gridContainer.append(cell);
+  }
+}
+
+function createNewSingleCell(index) {
   let cell = document.createElement("div");
   let ratio = document.createElement("div");
   let cellContent = document.createElement("div");
+
   cell.classList.add("cell");
   ratio.classList.add("ratio");
   cellContent.classList.add("cell-content");
-  cellContent.textContent = i + 1;
+
+  cellContent.textContent = index + 1;
   cell.append(cellContent);
-  gridContainer.append(cell);
+
+  return cell;
+}
+
+function addEventListenerToCells() {
+  let cells = document.querySelectorAll(".cell");
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", clickCell);
+  }
+}
+
+function clickCell() {
+  this.classList.add("active");
 }
